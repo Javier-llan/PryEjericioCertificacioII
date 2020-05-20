a9ab3943f101b3ecaf16d8a859f2c2a3462a6508
Javier Llango <jallango1@espe.edu.ec>
2020-05-20 11:14:11 -0500

    Ejercicio para certificacion II

---
+++ src/test/java/com/llango/ejercicio/PryEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llango.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
